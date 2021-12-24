3119c2efe4dfb85937db310c9e01fd247d106321
Bello Babakolo <fleetbeekay@gmail.com>
2021-12-25 00:50:14 +0100

    Perform cleanup

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card } from "react-native-paper";
-import styled from "styled-components";
+import styled from "styled-components/native";
 
 const RestaurantCard = styled(Card)`
   background-color: white;
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,36 +1,34 @@
 import React from "react";
-import { StatusBar, SafeAreaView, View } from "react-native";
-import styled from "styled-components";
+import { StatusBar, SafeAreaView } from "react-native";
+import styled from "styled-components/native";
 import RestaurantInfo from "../components/restaurant-info-card.component";
 import SearchInput from "../components/SearchInput";
 
-const RestaurantScreen = styled(SafeAreaView)`
+const SafeArea = styled(SafeAreaView)`
   flex: 1;
   background-color: white;
-  margin-top: ${String(StatusBar.currentHeight) + "px"};
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
-const SearchView = styled(View)`
+const SearchContainer = styled.View`
   padding: 16px;
 `;
 
-const RestaurantList = styled(View)`
+const RestaurantListContainer = styled.View`
   flex: 1;
   padding: 16px;
   background-color: blue;
 `;
 
-const RestaurantsScreen = () => {
-  return (
-    <RestaurantScreen>
-      <SearchView>
-        <SearchInput />
-      </SearchView>
-      <RestaurantList>
-        <RestaurantInfo />
-      </RestaurantList>
-    </RestaurantScreen>
-  );
-};
+const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <SearchInput />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfo />
+    </RestaurantListContainer>
+  </SafeArea>
+);
 
 export default RestaurantsScreen;

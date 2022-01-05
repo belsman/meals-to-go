2253f77a9614e58ce0aec4f5c2f666af9a03afd9
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-05 13:55:09 +0100

    Add fonts to the app

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { ThemeProvider } from "styled-components/native";
 
@@ -7,6 +13,18 @@ import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.sc
 import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>

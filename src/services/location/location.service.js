06daf526c724cac040b7e1ce2bd11d044b53be97
Bello Babakolo <fleetbeekay@gmail.com>
2022-04-25 22:28:07 +0100

    Setup geocoding

---
+++ src/services/location/location.service.js
@@ -0,0 +1,23 @@
+import camelize from "camelize";
+
+import { locations } from "./location.mock";
+
+export const locationRequest = (searchTerm) => {
+  return new Promise((resolve, reject) => {
+    const locationMock = locations[searchTerm];
+
+    if (!locationMock) {
+      reject("not found");
+    }
+
+    resolve(locationMock);
+  });
+};
+
+export const locationTransform = (result) => {
+  const formatedResult = camelize(result);
+  const { geometry = {} } = formatedResult.results[0];
+  const { lat, lng } = geometry.location;
+
+  return { lat, lng };
+};

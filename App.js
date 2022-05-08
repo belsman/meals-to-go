a1d5b27beef72f293b1a85b041ee9278e6425ff7
Bello Babakolo <fleetbeekay@gmail.com>
2022-05-08 23:02:43 +0100

    Set up Navigation structure

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import Ionicons from "@expo/vector-icons/Ionicons";
 
 import {
   useFonts as useOswald,
@@ -10,15 +9,12 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { ThemeProvider } from "styled-components/native";
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+
 import { theme } from "./src/infrastructure/theme";
 
-import { Text, View } from "react-native";
+import { Navigation } from "./src/infrastructure/navigation";
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -33,62 +29,17 @@ export default function App() {
     return null;
   }
 
-  const Tab = createBottomTabNavigator();
-
   return (
     <>
       <ThemeProvider theme={theme}>
         <LocationContextProvider>
           <RestaurantsContextProvider>
-            <NavigationContainer>
-              <Tab.Navigator
-                screenOptions={({ route }) => ({
-                  tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-
-                    if (route.name === "Restaurants") {
-                      iconName = focused ? "restaurant" : "restaurant-outline";
-                    } else if (route.name === "Map") {
-                      iconName = focused ? "map" : "map-outline";
-                    } else if (route.name === "Settings") {
-                      iconName = focused ? "settings" : "settings-outline";
-                    }
-
-                    return (
-                      <Ionicons name={iconName} size={size} color={color} />
-                    );
-                  },
-                  tabBarActiveTintColor: "tomato",
-                  tabBarInactiveTintColor: "gray",
-                  headerShown: false,
-                })}
-              >
-                <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-                <Tab.Screen name="Map" component={MapScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-              </Tab.Navigator>
-            </NavigationContainer>
+            <Navigation />
           </RestaurantsContextProvider>
         </LocationContextProvider>
       </ThemeProvider>
 
       <ExpoStatusBar style="auto" />
     </>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-function MapScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Google Maps!</Text>
-    </View>
   );
 }
---
+++ src/infrastructure/navigation/app.navigation.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+
+const Tab = createBottomTabNavigator();
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function MapScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Google Maps!</Text>
+    </View>
+  );
+}
+
+export const AppNavigation = () => (
+  <NavigationContainer>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Restaurants") {
+            iconName = focused ? "restaurant" : "restaurant-outline";
+          } else if (route.name === "Map") {
+            iconName = focused ? "map" : "map-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "settings-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
